36c1cff1c313d259ca9158021eb0abf91e50c496
Javier Islas <Dimcairion@live.com>
2022-10-01 16:57:25 -0300

    Initial Commit

---
+++ src/main/java/MAPAW/Proyecto/GymacionApplication.java
@@ -0,0 +1,13 @@
+package MAPAW.Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MAPAW/Proyecto/GymacionApplicationTests.java
@@ -0,0 +1,13 @@
+package MAPAW.Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
